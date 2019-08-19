d9832a91ce3cb748b7f5cbb60f1fd9f791960195
Damjan Dabovski <damjan.dabovski@students.finki.ukim.mk>
2019-08-19 23:08:09 +0200

    Refining base component scaffolding, set up Bootstrap.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { GameDashboardComponent } from './game-dashboard/game-dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:LoginComponent},
+  {path:"dashboard", component:GameDashboardComponent},
+  {path:"", redirectTo:"dashboard", pathMatch:"full"},
+  {path:"**", component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BoardComponent } from './board/board.component';
 import { UsersComponent } from './users/users.component';
 import { MyGamesComponent } from './my-games/my-games.component';
 import { GameDashboardComponent } from './game-dashboard/game-dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { GameDashboardComponent } from './game-dashboard/game-dashboard.componen
     BoardComponent,
     UsersComponent,
     MyGamesComponent,
-    GameDashboardComponent
+    GameDashboardComponent,
+    PageNotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
