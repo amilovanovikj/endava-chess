1ab3c3e0a1f162cb85caf7860c108ee9168c4ccc
Damjan Dabovski <damjan.dabovski@students.finki.ukim.mk>
2019-08-20 15:42:57 +0200

    Created users component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { MovesListComponent } from './moves-list/moves-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IUser } from '../models/IUser';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  selectSize = 3;
+  userList: IUser[];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get<IUser[]>('../assets/users_mock.json').subscribe(
+      response => {
+        this.userList = response;
+        console.log(this.userList);
+      }
+    );
   }
 
 }
