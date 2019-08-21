74f843f04b56950bfa67aeb6912990e910836efd
Damjan Dabovski <damjan.dabovski@students.finki.ukim.mk>
2019-08-21 19:49:29 +0200

    Finished game routing setup.

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { IGame } from '../models/IGame';
 
@@ -12,13 +12,27 @@ export class BoardComponent implements OnInit {
   game: IGame;
   id: number;
 
+  fetchGame(id: number): void{
+    this.http.get<IGame[]>('../assets/mygames_mock.json').subscribe(
+      response => {
+        this.game = response.find((game) => +game.id === id);
+      },
+      () => {},
+      () => {
+        //console.log(this.game);
+        if(!this.game){ this.router.navigate(['/dashboard/game/0'])}
+      }
+    );
+  }
+
   constructor(private http: HttpClient,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(
       params => {
         this.id = +params.get('id');
+        this.fetchGame(this.id);
       }
     );
     /* this.id = +this.route.snapshot.paramMap.get('id'); */
